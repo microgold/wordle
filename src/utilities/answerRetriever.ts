5cb952f49978286b06a15e7645c39be7809aeec7
mike <mike@microgold.com>
2022-08-19 12:44:56 -0500

    added multiple possible answers to wordle

--- src/utilities/answerRetriever.ts
+++ src/utilities/answerRetriever.ts
@@ -1,1 +1,12 @@
-export const retrieveAnswer = ():string => { return 'choir'}
+import { getwordlist } from "./wordlelist";
+
+export let answer = ''
+
+export const retrieveAnswer = ():string => {
+
+    if (answer.length > 0) return answer
+
+    const wordlelist = getwordlist()
+    answer = wordlelist[Math.floor(Math.random() * wordlelist.length)]; 
+    return answer 
+}
